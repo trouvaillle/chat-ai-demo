2f9e08c3b0667b3adfa36190742fecf284ddf6f4
seokmo.yoo <seokmo.yoo@navercorp.com>
2024-05-16 00:29:06 +0900

    fix: error

--- gpt4all/gpt4all-demo.py
+++ gpt4all/gpt4all-demo.py
@@ -1,5 +1,7 @@
 from gpt4all import GPT4All
 
 model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
-output = model.generate("The capital of France is ", max_tokens=3)
-print(output)
+while True:
+    prompt = input("You: ")
+    output = model.generate(prompt, max_tokens=3000)
+    print(output)
--- langchain/langchain-demo.py
+++ langchain/langchain-demo.py
@@ -2,7 +2,7 @@
 from langchain_community.llms import Ollama
 
 if __name__ == "__main__":
-    llm_model = Ollama(model="llama2")
+    llm_model = Ollama(model="llama3")
     while True:
         prompt=input("You: ")
         response=llm_model.invoke(prompt)
